797c1100bf963b2325daabf143e454894c60e0c4
Zeph Arvanitis <zeph.arvanitis@gmail.com>
2022-07-23 23:03:32 -0700

    Add module path to scrape.sh
    It now completes successfully!

--- scrape.sh
+++ scrape.sh
@@ -2,4 +2,5 @@ mwoffliner \
     --mwUrl https://handbook.mteriefire.com \
     --adminEmail zeph.arvanitis@gmail.com \
     --mwWikiPath "/w/" \
-    --mwApiPath "/api.php"
+    --mwApiPath "/api.php" \
+    --mwModulePath "/load.php"
